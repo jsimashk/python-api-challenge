da416cab825ba7be34f9b33b53d9c270f971a6ba
Jana Simashkevich <jsimashk@gmail.com>
2019-11-07 21:00:29 -0500

    updated homework

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,2 +1,6 @@
 # OpenWeatherMap API Key
-api_key = "18c1679263c0ac6784dd15e5e1fd5cdc"
+#api_key = "18c1679263c0ac6784dd15e5e1fd5cdc"
+api_key = "b61c9ba39579dc8adf941f5bb864c6df"
+#api_key = "5e7b51def9f8ae78a18ac744f956dfd3"
+
+
